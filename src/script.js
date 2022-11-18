617b5758a489ba15df8fbb5223eb94bc743cd102
dzmoney <115180206+dzmoney@users.noreply.github.com>
2022-11-18 10:34:14 -0500

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,74 @@
+//Current Time
+let now = new Date();
+
+let currentDate = document.querySelector("#current-date");
+let currentTime = document.querySelector("#current-time");
+
+let date = now.getDate();
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let mins = now.getMinutes();
+if (mins < 10) {
+  mins = `0${mins}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+currentDate.innerHTML = `${day} ${date}`;
+currentTime.innerHTML = `${hour}:${mins}`;
+
+//Display Temp
+
+function getSearchWeather(response) {
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  document.querySelector("#current-city-temp").innerHTML = `${temperature}°C`;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "652f995668f5749b34360880cf6212e7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getSearchWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchCity(city);
+}
+
+function searchCurrentLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "652f995668f5749b34360880cf6212e7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(getSearchWeather);
+}
+
+function getCurrentLocationWeather(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchCurrentLocation);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocationWeather);
+
+searchCity("Toronto");
